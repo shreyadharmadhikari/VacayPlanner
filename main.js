f191aa6b270d1dd6b3f6e975e9712a2d46ed542a
Shreya Dharmadhikari <68768718+shreyadharmadhikari@users.noreply.github.com>
2026-04-22 22:21:06 +0530

    Remove duplicate weatherHTML variable declaration
    Removed redundant declaration of weatherHTML variable.

--- main.js
+++ main.js
@@ -738,8 +738,6 @@ async function fetchDestinationUnifiedFunc(userInput) {
 
     /******** WEATHER (ONLY CITY) ********/
     let weatherHTML = "";
-
-    let weatherHTML = "";
     let countryHTML = "";
 
     /* ---------------- CITY ---------------- */
